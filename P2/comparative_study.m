3281f1afd6553bfccd7df1eb7f74e5a7587fed77
hanon11 <lucia.atienza2@gmail.com>
2022-03-24 13:45:34 +0100

    P2

--- P2/comparative_study.m
+++ P2/comparative_study.m
@@ -1,7 +1,6 @@
 N = 12;
 mat = zeros(3, 3);
-mean_itera = 0;
-n_times = 0;
+
 
 tStart = tic;    
 mean_itera = 0;
@@ -9,12 +8,13 @@ n_times = 0;
 for i=1:1:1000
     rng(i);
     [C, itera] = localSearch(N);
-    mean_itera = mean_itera+itera;
+    
     if(C == 0)
         n_times=n_times+1;
+        mean_itera = mean_itera+itera;
     end
 end
-mat(2,1) = toc(tStart);
+mat(2,1) = toc(tStart)/1000;
 mat(1,1) = n_times;
 mat(3,1) = mean_itera/1000;
 
@@ -24,12 +24,13 @@ n_times = 0;
 for i=1:1:1000
     rng(i);
     [sol, C, itera] = SA(N);
-    mean_itera = mean_itera+itera;
+    
     if(C == 0)
         n_times=n_times+1;
+        mean_itera = mean_itera+itera;
     end
 end
-mat(2,2) = toc(tStart);
+mat(2,2) = toc(tStart)/1000;
 mat(1,2) = n_times;
 mat(3,2) = mean_itera/1000;
 
@@ -39,12 +40,13 @@ n_times = 0;
 for i=1:1:1000
     rng(i);
     [sol, C, itera] = taboo(N);
-    mean_itera = mean_itera+itera;
+    
     if(C == 0)
         n_times=n_times+1;
+        mean_itera = mean_itera+itera;
     end
 end
-mat(2,3) = toc(tStart);
+mat(2,3) = toc(tStart)/1000;
 mat(1,3) = n_times;
 mat(3,3) = mean_itera/1000;
 
@@ -59,12 +61,13 @@ n_times = 0;
 for i=1:1:1000
     rng(i);
     [C, itera] = localSearch(N);
-    mean_itera = mean_itera+itera;
+    
     if(C == 0)
         n_times=n_times+1;
+        mean_itera = mean_itera+itera;
     end
 end
-mat2(2,1) = toc(tStart);
+mat2(2,1) = toc(tStart)/1000;
 mat2(1,1) = n_times;
 mat2(3,1) = mean_itera/1000;
 
@@ -74,12 +77,13 @@ n_times = 0;
 for i=1:1:1000
     rng(i);
     [sol, C, itera] = SA(N);
-    mean_itera = mean_itera+itera;
+    
     if(C == 0)
         n_times=n_times+1;
+        mean_itera = mean_itera+itera;
     end
 end
-mat2(2,2) = toc(tStart);
+mat2(2,2) = toc(tStart)/1000;
 mat2(1,2) = n_times;
 mat2(3,2) = mean_itera/1000;
 
@@ -89,12 +93,13 @@ n_times = 0;
 for i=1:1:1000
     rng(i);
     [sol, C, itera] = taboo(N);
-    mean_itera = mean_itera+itera;
+    
     if(C == 0)
         n_times=n_times+1;
+        mean_itera = mean_itera+itera;
     end
 end
-mat2(2,3) = toc(tStart);
+mat2(2,3) = toc(tStart)/1000;
 mat2(1,3) = n_times;
 mat2(3,3) = mean_itera/1000;
 
---
+++ seminario3.1/tournament_select.m
@@ -0,0 +1,14 @@
+function parents = tournament_select(poblacion, k)
+    tam = width(poblacion);
+    parents = zeros(2, tam);
+    for i=1:1:tam*2
+        selected = zeros(1,k);
+        costes = zeros(1,k);
+        for j=1:1:k
+            selected(j) = randi(k);
+            costes(j) = fEval(poblacion(j,:));
+        end
+        
+    end
+    
+end
